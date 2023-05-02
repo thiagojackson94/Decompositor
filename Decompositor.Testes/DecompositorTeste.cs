8996420e1b4ff31e2d7814f8024d9bf5a3fbf1bc
Thiago <thiagojacksonrs@gmail.com>
2023-05-02 00:09:07 -0300

    ajuste testes

--- Decompositor.Testes/DecompositorTeste.cs
+++ Decompositor.Testes/DecompositorTeste.cs
@@ -16,25 +16,25 @@ namespace Decompositor.Testes
         [Test]
         public void DecomporNum10()
         {
-            var respostaEsperada = "Número de Entrada: 10\r\nDivisores: 1, 2, 5, 10\r\nDivisores Primos: 2, 5\r\n";
+            var respostaEsperada = "Número de Entrada: 10\nDivisores: 1, 2, 5, 10\nDivisores Primos: 2, 5\n";
             var resultado = Decompositor.DecomporNumero("10");
             Assert.AreEqual(respostaEsperada, resultado);
         }
 
-        //[Test]
-        //public void DecomporLetras()
-        //{
-        //    var respostaEsperada = "O número informado deve ser um número positivo.";
-        //    var resultado = Decompositor.DecomporNumero("ABC");
-        //    Assert.AreEqual(respostaEsperada, resultado);
-        //}
+        [Test]
+        public void DecomporLetras()
+        {
+            var respostaEsperada = "O número informado deve ser um número positivo.";
+            var resultado = Decompositor.DecomporNumero("ABC");
+            Assert.AreEqual(respostaEsperada, resultado);
+        }
 
-        //[Test]
-        //public void DecomporNumNegativo()
-        //{
-        //    var respostaEsperada = "O número informado deve ser um número positivo.";
-        //    var resultado = Decompositor.DecomporNumero("-10");
-        //    Assert.AreEqual(respostaEsperada, resultado);
-        //}
+        [Test]
+        public void DecomporNumNegativo()
+        {
+            var respostaEsperada = "O número informado deve ser um número positivo.";
+            var resultado = Decompositor.DecomporNumero("-10");
+            Assert.AreEqual(respostaEsperada, resultado);
+        }
     }
 }
